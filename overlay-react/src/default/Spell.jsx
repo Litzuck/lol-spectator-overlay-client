6dd8ea755794a1ae9c19b3c87a689d387216b926
litzuck <o.j.t@online.de>
2022-05-17 23:46:17 +0200

    fix missing barrier summoner spell

--- overlay-react/src/default/Spell.jsx
+++ overlay-react/src/default/Spell.jsx
@@ -1,8 +1,6 @@
 import React from "react"
 import "./style/pick.css"
 
-import cx from "classnames"
-
 export default class Spell extends React.Component {
 
     static defaultProps={
@@ -10,7 +8,7 @@ export default class Spell extends React.Component {
     }
 
     render(){
-        if(this.props.spellId>0 && this.props.spellId<20)
+        if(this.props.spellId>0 && this.props.spellId<30)
             return <img className="spell" src={require("../assets/summoner-spells/" + this.props.spellId + ".png").default} alt="" />
         else
             return <img className=""  alt="" />
